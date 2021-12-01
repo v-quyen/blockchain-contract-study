58c6cf6d7b5e33f3034629dd103e5f88c952ff0b
クイン｜quyen <que.quyen.vuong@10antz.co.jp>
2021-12-01 18:28:33 +0900

    init commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+    string private message;
+
+    function contructor(string memory mess) public {
+        message = mess;
+    }
+
+    function SetMessage(string memory mess) public {
+        message = mess;
+    }
+
+    function GetMessage() public view returns (string memory) {
+        return message;
+    }
+}
